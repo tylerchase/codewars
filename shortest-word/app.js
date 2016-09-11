48bfdf6beb52bde4b9f4f44b6f1f14cc93c0cf28
Tyler Chase <Tyler@Tylers-MacBook-Air.local>
2016-09-11 14:51:16 -0600

    shortest word

---
+++ shortest-word/app.js
@@ -0,0 +1,15 @@
+// x Simple, given a string of words, return the length of the shortest word(s).
+//
+// String will never be empty and you do not need to account for different data types.
+
+// my answer
+function findShort(s){
+var split = s.split(' ')
+counter = 100
+for (var i=0; i<split.length; i++){
+  if (split[i].length < counter){
+  	counter = split[i].length
+  	}
+  }
+  return counter;
+}
